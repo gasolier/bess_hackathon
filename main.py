706ce530798417f1a26a7f7f0179a0fba915bcd1
Owen Chandler <ochandle@Owens-MacBook-Pro.local>
2019-07-22 13:27:26 +1000

    reading data and pushing to hip tests

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from readdicom import process
+import hipTests
+import writeLogs
 import pydicom
 import argparse
-import sys
 import os
+
 
 # Parse the arguments
 ap = argparse.ArgumentParser()
@@ -12,16 +14,35 @@ args = vars(ap.parse_args())
 dir_path = args["directory"]
 
 # array that stores that data from all DICOM files in the given directory
-data = []
+patientData = {
+    "hipScans" : [],
+    "kneeScans" : [],
+    "ankleScans" : []
+}
 
-# Cycle through files in directory and pass to read_dicom
-for root, dirs, files in os.walk(dir_path, topdown=False):    
-    for name in files:
-        path = os.path.join(root, name)
-        data.append(process(path))
-    
-    # for name in dirs:
-    #     path = os.path.join(root, name)
-    #     process(path)
+# go through the sub-directories and read the data in these into our
+# patientData variable
+for root, dirs, _ in os.walk(dir_path, topdown=False):    
+    for d in dirs:
+        next_down = os.path.join(dir_path, d)
+        for _, _, files in os.walk(next_down):
+            for f in files:
+                # check our folder type
+                if d == "Knee":
+                    patientData["kneeScans"].append({'filename' : f, 'data' : process(os.path.join(next_down, f))})
+                elif d == "Hip":
+                    patientData["hipScans"].append({'filename' : f, 'data' : process(os.path.join(next_down, f))})
+                elif d == "Ankle":
+                    patientData["ankleScans"].append({'filename' : f, 'data' : process(os.path.join(next_down, f))})
 
-print(data)
+# run the test
+for scanType in patientData.keys():
+    for scan in patientData[scanType]:
+        print(f"testing {scan['filename']}")
+        if scanType == "hipScans":
+            print("testing hip")
+            hipTests.runTests(scan['data'])
+        elif scanType == "kneeScans":
+            print("testing knees")
+        elif scanType == "ankleScans":
+            print("testing ankles")
--- readdicom.py
+++ readdicom.py
@@ -3,12 +3,12 @@ import pydicom
 def process (file_path):
     dataset  = pydicom.dcmread(file_path)
 
+    # print(dataset.elements)
+
     # for now just return the pixel data and spacing in a dictionary
     return {
-        "filename" : file_path,
-        "body_part" : dataset.BodyPartExamined,
         "slice_thickness" : dataset.SliceThickness,
         "slice_location" : dataset.SliceLocation,
-        #"pixel_array" : dataset.PixelArray,
+        "pixel_array" : dataset.PixelData,
         "pixel_spacing" : dataset.PixelSpacing
     }
