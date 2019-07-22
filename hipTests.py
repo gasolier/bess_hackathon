3c45154a389f9ff0770256a3656524ede57c66f9
ZJamesZhang <50870568+ZJamesZhang@users.noreply.github.com>
2019-07-22 13:36:35 +1000

    added code

--- hipTests.py
+++ hipTests.py
@@ -1,8 +1,13 @@
-def slice_test(dcm_data):
-    result = 0
+def runTests(dcm_data):
     # # Pass/Fail if criteria = 0; pass, else if criteria = 1 then incorrect slice thickness
     # criteria = 0 
     # ss = dataset.SliceThickness
     # if ss < 1.25 and ss > 2:
     #     criteria = 1
+    result = 0
+    ss = dcm_data["slice_thickness"]
+    
+    if ss < 1.25 and ss > 2:
+        result = 1
+
     return result
