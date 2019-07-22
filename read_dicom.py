9bd54933d86f2a05c38a0e8c237cf275410f0993
Owen Chandler <ochandle@Owens-MacBook-Pro.local>
2019-07-22 10:43:48 +1000

    started reading all from directory

--- read_dicom.py
+++ read_dicom.py
@@ -1,5 +1,6 @@
 import pydicom
 import numpy as np
+import os
 
 def read_dicom (file_path):
     dataset  = pydicom.dcmread(file_path)
@@ -10,3 +11,12 @@ def read_dicom (file_path):
         "pixel_spacing" : dataset.PixelSpacing
     }
 
+def read_from_directory (dir_path):
+    print(dir_path)
+    dicom_arr = []
+    for root, dirs, files in os.walk(dir_path):
+        print(root, files)
+        for f in files:
+            print(root + f)
+            dicom_arr.append(read_dicom(root + f))
+    return dicom_arr
